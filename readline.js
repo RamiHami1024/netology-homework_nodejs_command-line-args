1e4fdfd39293aa729d617019561db24d76e67d78
RamiHami <test@test.com>
2022-08-14 20:50:56 +0300

    readline.js

---
+++ readline.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+// Task #2
+
+const readline = require('readline')
+const { stdin: input, stdout: output } = require('process');
+const { rmSync } = require('fs');
+const rl = readline.createInterface({ input, output });
+const number = Math.floor(Math.random() * 100);
+let guess = 0;
+let counter = 0;
+console.log(number);
+
+rl.question('Загадано число в диапазоне от 0 до 100: ', (answer) => {
+    guess = Number(answer);
+
+    if (guess > number) {
+        console.log('Меньше')
+    } else if (guess < number) {
+        console.log('Больше')
+    } else {
+        console.log('Вы угадали')
+        rl.close()
+    }
+
+    rl.on('line', (answer) => {
+        guess = Number(answer);
+
+        if (guess > number) {
+        console.log('Меньше')
+        } else if (guess < number) {
+            console.log('Больше')
+        } else {
+            console.log('Вы угадали')
+            rl.close()
+        }
+    })
+});
+
